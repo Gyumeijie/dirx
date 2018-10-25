ce1738e2de19e97c698408febf0ea2a0d4e44224
Gyumeijie <18170879819@163.com>
2018-10-25 14:38:36 +0800

    feat: add cli options
    dirx-cli install
    dirx-cli set-strategy "strategy"

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #! /bin/bash
 
-cat config.json | grep '"installed" *: *"no"' >&/dev/null
+function printUsage() {
+   echo 'Usage:' 
+   echo '      dirx-cli install'
+   echo '      dirx-cli set-strategy strategy("frequency" or "accessTime")'
+}
 
-if [ $? = 0 ]; then
-   # Change install status to "yes"
-   sed -i 's/\("installed" *\): *"\([a-z]\+\)"/\1: "yes"/g' config.json
+if [ $# -eq 0 ]; then
+   printUsage
+elif [ "$1" = "install" ]; then
+   cat config.json | grep '"installed" *: *"no"' >&/dev/null
 
-   # Change {INSTALL_PATH}
-   sed -i "s:{INSTALL_PATH}:$PWD:" interceptor.sh
+   if [ $? = 0 ]; then
+      # Change install status to "yes"
+      sed -i 's/\("installed" *\): *"\([a-z]\+\)"/\1: "yes"/g' config.json
 
-   if [ -f ~/.zshrc ]; then
-      cat ./interceptor.bash >> ~/.zshrc
+      # Change {INSTALL_PATH}
+      sed -i "s:{INSTALL_PATH}:$PWD:" interceptor.bash
+      sed -i "s:{INSTALL_PATH}:$PWD:" interceptor.zsh
+
+      if [ -f ~/.zshrc ]; then
+         cat ./interceptor.zsh >> ~/.zshrc
+      fi
+
+      if [ -f ~/.bashrc ]; then
+         cat ./interceptor.bash >> ~/.bashrc
+      fi
    fi
-
-   if [ -f ~/.bashrc ]; then
-      cat ./interceptor.zsh >> ~/.bashrc
-   fi
-fi
+elif [[ "$1" = "set-strategy" && ("$2" = "frequency" || $2 = "accessTime" ) ]]; then
+   # Change strategy
+   sed -i "s/\(\"defalutStrategy\" *\): *\"\([[:alpha:]]\+\)\"/\1: \"$2\"/g" config.json
+else
+   printUsage
+fi 
